1998e4ba7a02bf22722cbf2d937d0f86180e0a85
Cesar <cesar_silk321@hotmail.com>
2020-12-26 23:15:50 -0300

    Authenticacao

--- app/Http/Middleware/JwtMiddleware.php
+++ app/Http/Middleware/JwtMiddleware.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\User;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Tymon\JWTAuth\Facades\JWTAuth;
 use Tymon\JWTAuth\Http\Middleware\BaseMiddleware;
 
@@ -11,7 +13,17 @@ class JwtMiddleware extends BaseMiddleware
 {
     public function handle(Request $request, Closure $next)
     {
-        $user = JWTAuth::parseToken()->authenticate();
+
+        $token = JWTAuth::getToken();
+        $user = JWTAuth::getPayload($token)->toArray();
+        if (isset($user)) {
+            $st = new User();
+            $st->fill($user);
+            Auth::setUser($st);
+            return $next($request);
+        }
+
+//        $user = JWTAuth::parseToken()->authenticate();
         return $next($request);
     }
 }
