0aaecaf48bca6f2288d94c439253ddbb7e7a8a8b
Cesar <cesar_silk321@hotmail.com>
2020-12-26 21:31:03 -0300

    Auth Routes

--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -5,12 +5,21 @@ namespace App\Http\Controllers\Api\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Tymon\JWTAuth\JWTAuth;
 
 class AuthController extends Controller
 {
     public function login(Request $request)
     {
 
-        return response()->json(['sdfasdfasdf'], 200);
+        $auth = Auth::attempt(['email' => $request->get('username'), 'password' => 'password']);
+        if (!$auth) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Invalid Email or Password',
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+        return response()->json([$auth], 200);
     }
 }
